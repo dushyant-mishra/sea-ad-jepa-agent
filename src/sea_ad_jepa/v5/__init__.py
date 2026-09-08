ed15904ed2e0bfb6964dd3d9c48f5975d4b58a53
Dushyant Mishra, PhD <dushyant.mishra@gmail.com>
2026-09-08 13:17:01 -0400

    teacher-student v5: add data-first execution primitives

---
+++ src/sea_ad_jepa/v5/__init__.py
@@ -0,0 +1,16 @@
+"""Prospective data-first Teacher/Student V5 mechanics.
+
+Nothing in this package is execution authority.
+"""
+from .data_first_geometry import (
+    PackedValidTokens, anchored_triplet_capacity, partial_fine_derangement,
+    evidence_telemetry,
+    ragged_relational_support,
+    pack_valid_tokens,
+    operator_homogeneous_microbatch_plan,
+    mean_loss_weight,
+)
+__all__=[
+    'PackedValidTokens','anchored_triplet_capacity','partial_fine_derangement','evidence_telemetry','ragged_relational_support',
+    'pack_valid_tokens','operator_homogeneous_microbatch_plan','mean_loss_weight'
+]
