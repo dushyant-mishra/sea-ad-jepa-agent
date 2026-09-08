ed19c6a4c1c298bb406874c75efbb0777eef55ec
Dushyant Mishra, PhD <dushyant.mishra@gmail.com>
2026-09-08 14:03:45 -0400

    teacher-student v5: export reviewed planning primitives

--- src/sea_ad_jepa/v5/__init__.py
+++ src/sea_ad_jepa/v5/__init__.py
@@ -3,14 +3,39 @@
 Nothing in this package is execution authority.
 """
 from .data_first_geometry import (
-    PackedValidTokens, anchored_triplet_capacity, partial_fine_derangement,
+    PackedValidTokens,
+    anchored_triplet_capacity,
     evidence_telemetry,
-    ragged_relational_support,
-    pack_valid_tokens,
-    operator_homogeneous_microbatch_plan,
     mean_loss_weight,
+    operator_homogeneous_microbatch_plan,
+    pack_valid_tokens,
+    partial_fine_derangement,
+    ragged_relational_support,
 )
+from .finite_triplets_v1 import sample_finite_anchored_triplets
+from .keyed_rng_v1 import KeyedDropoutSpec, keyed_dropout, keyed_dropout_mask
+from .scientific_estimand_v1 import (
+    EstimandPolicy,
+    GroupCount,
+    group_masses,
+    importance_diagnostics,
+)
+
 __all__=[
-    'PackedValidTokens','anchored_triplet_capacity','partial_fine_derangement','evidence_telemetry','ragged_relational_support',
-    'pack_valid_tokens','operator_homogeneous_microbatch_plan','mean_loss_weight'
+    "PackedValidTokens",
+    "anchored_triplet_capacity",
+    "evidence_telemetry",
+    "mean_loss_weight",
+    "operator_homogeneous_microbatch_plan",
+    "pack_valid_tokens",
+    "partial_fine_derangement",
+    "ragged_relational_support",
+    "sample_finite_anchored_triplets",
+    "KeyedDropoutSpec",
+    "keyed_dropout",
+    "keyed_dropout_mask",
+    "EstimandPolicy",
+    "GroupCount",
+    "group_masses",
+    "importance_diagnostics",
 ]
