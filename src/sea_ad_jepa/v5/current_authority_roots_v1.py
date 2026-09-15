5c0ca01d4c851688b17c4fad47899c94c0228ec8
Dushyant Mishra, PhD <dushyant.mishra@gmail.com>
2026-09-15 17:25:33 -0400

    v5: define exact current authority root vocabulary

---
+++ src/sea_ad_jepa/v5/current_authority_roots_v1.py
@@ -0,0 +1,19 @@
+"""Exact current-V5 authority root vocabulary."""
+CURRENT_V5_UPSTREAM_AUTHORITY_ROOTS = (
+    "full104_substrate_sha256",
+    "representation_authority_sha256",
+    "base_training_estimand_sha256",
+    "teacher_target_semantics_sha256",
+    "target_address_query_authority_sha256",
+    "masking_authority_sha256",
+    "model_geometry_authority_sha256",
+    "schedule_authority_sha256",
+    "ema_authority_sha256",
+    "anti_cheat_authority_sha256",
+    "runtime_source_sha256",
+)
+CURRENT_V5_RECEIPT_AUTHORITY_ROOTS = (
+    *CURRENT_V5_UPSTREAM_AUTHORITY_ROOTS[:-1],
+    "preexecution_authority_sha256",
+    CURRENT_V5_UPSTREAM_AUTHORITY_ROOTS[-1],
+)
